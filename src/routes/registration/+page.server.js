e57a77ac8bd434fbee9fd63f8f8d4f1a4546f30e
Hal Helms <hal.helms@gmail.com>
2024-07-22 08:58:13 -0400

    VIEW registration screen - default action

--- src/routes/registration/+page.server.js
+++ src/routes/registration/+page.server.js
@@ -1,3 +1,11 @@
 export async function load() {
   return {};
 };
+
+
+export const actions = {
+  default: async ({ cookies, request }) => {
+    const data = await request.formData();
+    const formData = Object.fromEntries(data);
+  }
+};
