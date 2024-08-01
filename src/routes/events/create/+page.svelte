97503381d8ac1118d55dacebb0b0a638aff4a6ec
Hal Helms <hal.helms@gmail.com>
2024-08-01 13:37:51 -0400

    Event creation form WIP 3

--- src/routes/events/create/+page.svelte
+++ src/routes/events/create/+page.svelte
@@ -4,51 +4,52 @@
 </script>
 
 <main>
+  <h1 class="text-center text-2xl font-bold">Create Event</h1>
   
   <form use:enhance method="post">
   
-  <div class="px-4">
-    <label for="name" class="text-xs">Event name</label>
+  <div class="px-4 mt-2">
+    <label for="name" class="block">Event name</label>
     <input type="text" id="name" name="name" />
   </div>
   
-  <div class="px-4">
-    <label for="start_date" class="text-xs">Start date</label>
+  <div class="px-4 mt-2">
+    <label for="start_date" class="">Start date</label>
     <input type="date" id="start_date" name="start_date" />
   </div>
   
-  <div class="px-4">
-    <label for="start_time" class="text-xs">Start time</label>
+  <div class="px-4 mt-2">
+    <label for="start_time" class="">Start time</label>
     <input type="time" id="start_time" name="start_time" />
   </div>
   
-  <div class="px-4">
-    <label for="end_date" class="text-xs">End date</label>
+  <div class="px-4 mt-2">
+    <label for="end_date" class="">End date</label>
     <input type="date" id="end_date" name="end_date" />
   </div>
   
-  <div class="px-4">
-    <label for="end_time" class="text-xs">End time</label>
+  <div class="px-4 mt-2">
+    <label for="end_time" class="">End time</label>
     <input type="time" id="end_time" name="end_time" />
   </div>
   
-  <div class="px-4">
-    <label for="cutoff_date" class="text-xs">Cutoff date</label>
+  <div class="px-4 mt-2">
+    <label for="cutoff_date" class="">Cutoff date</label>
     <input type="date" id="cutoff_date" name="cutoff_date" />
   </div>
 
-  <div class="px-4">
-    <label for="event_details" class="text-xs">Event details</label>
+  <div class="px-4 mt-2">
+    <label for="event_details" class="">Event details</label>
     <textarea id="event_details" name="event_details"></textarea>
   </div>
 
-  <div class="px-4">
-    <label for="location" class="text-xs">Event location</label>
+  <div class="px-4 mt-2">
+    <label for="location" class="">Event location</label>
     <textarea id="location" name="location"></textarea>
   </div>
  
-  <div class="px-4">
-    <label for="categories" class="text-xs">Categories</label>
+  <div class="px-4 mt-2">
+    <label for="categories" class="">Categories</label>
   </div>
 
   <div class="px-4">
@@ -71,23 +72,23 @@
     <label for="get-together">Get-together</label>
   </div>
     
-  <div class="px-4">
-    <label for="price" class="">Price</label>
+  <div class="px-4 mt-2">
+    <label for="price" class="block">Price</label>
     <input type="number" id="price" name="price" />
   </div>
     
-  <div class="px-4">
-    <label for="max_attendees" class="">Max attendees</label>
+  <div class="px-4 mt-2">
+    <label for="max_attendees" class="block">Maximum attendees</label>
     <input type="number" id="max_attendees" name="max_attendees" />
   </div>
     
-  <div class="px-4">
+  <div class="px-4 mt-2">
     <label for="access" class="">Access</label>
   </div>
 
   <div class="px-4">
     <input type="radio" id="public" name="access" />
-    <label for="public">Party</label>
+    <label for="public">Public</label>
   </div>
 
   <div class="px-4">
@@ -99,8 +100,6 @@
     <input type="radio" id="vip" name="access" />
     <label for="vip">VIP members only</label>
   </div>
-
-
 
   <div class="flex justify-center mt-3">
   <button>Create Event!</button>
@@ -117,5 +116,9 @@
 
   button {
     @apply bg-blue-500 text-white font-bold py-2 px-4 text-center rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50;
+  }
+
+  label {
+    @apply font-semibold;
   }
 </style>
